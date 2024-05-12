03efb1ea32392dbb619bd1fda51f97ba1693ecf4
D-il <weligalagedilu@gmail.com>
2024-05-12 21:34:16 +0530

    7th commit to the task app

--- app/src/main/java/com/example/taskmanagementapp/Launch.kt
+++ app/src/main/java/com/example/taskmanagementapp/Launch.kt
@@ -1,11 +1,22 @@
 package com.example.taskmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 class Launch: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
     }
+
+    fun openmain(view: View) {
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+
+    }
+
 
 }
